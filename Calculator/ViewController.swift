ab6ece601f0e22fab6c285031ffb2583076aba38
DongHyunW0n <68365742+DongHyunW0n@users.noreply.github.com>
2022-08-28 14:21:38 +0900

    .

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -7,13 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum Operation{
+    case Add
+    case Subtract
+    case Divide
+    case Multiply
+    case UnKnown
+}
 
+class ViewController: UIViewController {
+    @IBOutlet weak var numberOutPutLabel: UILabel!
+    
+    var displayNumber = ""
+    var firstOperand = ""
+    var secondOperand = ""
+    var result = ""
+    var currentOperation: Operation = .UnKnown
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tabNumberButton(_ sender: UIButton) {
+        guard let numberValue = sender.title(for: .normal) else { return}
+        if self.displayNumber.count < 9 {
+            self.displayNumber += numberValue
+            self.numberOutPutLabel.text = self.displayNumber
+        }
+    }
+    
+    @IBAction func tabClearButton(_ sender: UIButton) {
+        
+    }
+    @IBAction func tabDotButton(_ sender: UIButton) {
+    }
+    @IBAction func tabDevideButton(_ sender: UIButton) {
+    }
+    @IBAction func tabMultiplyButton(_ sender: UIButton) {
+    }
+    @IBAction func tabSubtractButton(_ sender: UIButton) {
+    }
+    @IBAction func tabAddButton(_ sender: UIButton) {
+    }
+    @IBAction func tabEqualButtomn(_ sender: UIButton) {
+    }
 }
 
